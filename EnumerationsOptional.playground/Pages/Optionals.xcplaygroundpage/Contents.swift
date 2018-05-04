c8fd74c77db5e617c4eb18ea9ae2231a9dcf604f
tdimnet <thomas.dimnet@gmail.com>
2018-05-04 15:23:22 +0200

    Add optional binding unless of force unwrapping

--- EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -16,7 +16,33 @@ struct Person {
 let me: Person = Person(firstName: "Thomas", middleName: nil, lastName: "Dimnet")
 me.fullName()
 
+// Optional binding using if let
+let airportCodes = ["CDG": "Charles de Gaulle"]
+let newYorkAirport = airportCodes["JFK"]
 
+if let newYorkAirtport = airportCodes["JFK"] {
+    print(newYorkAirtport)
+} else {
+    print("Whoops! That key does not exist!")
+}
+
+let weatherDictionary: [String: [String: String]] = [
+    "currently": ["temperature": "23.3"],
+    "daily": ["temperature": "23.3"],
+    "weekly": ["temperature": "23.3"]
+]
+
+// Bad practice
+    //if let dailyWeather = weatherDictionary["daily"] {
+    //    if let highTemp = dailyWeather["temperature"] {
+    //        print(highTemp)
+    //    }
+    //}
+
+// Good practice
+if let dailyWeather = weatherDictionary["daily"], let highTemperature = dailyWeather["temperature"] {
+    print(highTemperature)
+}
 
 
 
