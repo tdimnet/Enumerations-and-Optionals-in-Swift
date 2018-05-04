4ae7fe4681cdceab755b70c60d217c6a9b989cc0
tdimnet <thomas.dimnet@gmail.com>
2018-05-04 12:30:00 +0200

    Add method to enum in order to generate colors.

--- EnumerationsOptional.playground/Pages/Course.xcplaygroundpage/Contents.swift
+++ EnumerationsOptional.playground/Pages/Course.xcplaygroundpage/Contents.swift
@@ -61,12 +61,22 @@ let type = dayType(for: Day.saturday)
 let isMutedType = isNotificationMuted(on: type)
 
 
+import UIKit
 enum ColorComponent {
-    case rgb(Float, Float, Float, Float)
-    case hsb(Float, Float, Float, Float)
+    case rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    case hsb(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat)
+    
+    func color() -> UIColor {
+        switch self {
+        case .rgb(let red, let green, let blue, let alpha):
+            return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+        case .hsb(let hue, let saturation, let brightness, let alpha):
+            return UIColor(hue: hue/360.0, saturation: saturation/100.0, brightness: brightness/100.0, alpha: alpha)
+        }
+    }
 }
 
-ColorComponent.rgb(61.0, 120.0, 198.0, 1.0)
+ColorComponent.rgb(red: 61.0, green: 120.0, blue: 198.0, alpha: 1.0).color()
 
 
 
