fd723cb5bf47773f687c8f985cb6c9bb210b945d
tdimnet <thomas.dimnet@gmail.com>
2018-05-04 11:18:18 +0200

    Add project skelethon with a few functions and harcoded data.

--- EnumerationsOptional.playground/Contents.swift
+++ EnumerationsOptional.playground/Contents.swift
@@ -1,7 +1,26 @@
+let week: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+func dayType(for day: String) -> String {
+    switch day {
+    case "Saturday", "Sunday":
+        return "Weekend"
+    case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+        return "Weekday"
+    default:
+        return "This is not a valid day"
+    }
+}
 
+func isNotificationMuted(on day: String) -> Bool {
+    if day == "Weekend" {
+        return true
+    } else {
+        return false
+    }
+}
 
-
+let result: String = dayType(for: week[4])
+let isMuted: Bool = isNotificationMuted(on: result)
 
 
 
