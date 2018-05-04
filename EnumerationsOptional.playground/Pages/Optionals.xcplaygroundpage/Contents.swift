cdb2c0b2febfcd3b366f63344852d322e51fe1de
tdimnet <thomas.dimnet@gmail.com>
2018-05-04 15:08:20 +0200

    Add first challenge and method for the Person Struct.

--- EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -2,11 +2,19 @@ struct Person {
     let firstName: String
     let middleName: String?
     let lastName: String
+    
+    func fullName() -> String {
+        if middleName == nil {
+            return firstName + " " + lastName
+        } else {
+            // return firstName + " " + middleName! + " " + lastName -> never force unwrapping like middleName!
+            return firstName + " " + middleName! + " " + lastName
+        }
+    }
 }
 
 let me: Person = Person(firstName: "Thomas", middleName: nil, lastName: "Dimnet")
-
-
+me.fullName()
 
 
 
---
+++ EnumerationsOptional.playground/Pages/OptionalsChallenges.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+// Challenges
+
+
+// 1.
+/*
+ Declare a variable named someValue of type optional Int and assign nil.
+ Note: You will have to explicitly declare the type as an optional Int otherwise the compiler cannot infer it.
+ */
+var someValue: Int? = nil
