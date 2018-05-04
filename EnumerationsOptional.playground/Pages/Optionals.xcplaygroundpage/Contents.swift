b1e833d777dd4a8fc2fd21c63b99b1d97488d523
tdimnet <thomas.dimnet@gmail.com>
2018-05-04 14:55:03 +0200

    Rename playground pages and add the new one which deals with optionals.

---
+++ EnumerationsOptional.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+struct Person {
+    let firstName: String
+    let middleName: String?
+    let lastName: String
+}
+
+let me: Person = Person(firstName: "Thomas", middleName: nil, lastName: "Dimnet")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
